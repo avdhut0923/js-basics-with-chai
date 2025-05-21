3374d01a29330876a2659110fa3012719c1dceb1
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-05-21 06:36:12 +0000

    day 2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 2323
+let accountEmail = "jaiHari@gmail.com"
+var accountPassword = "323223"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+// accountId=3322
+
+accountEmail="panduranga@gmail.com"
+accountPassword="223322"
+accountCity = "wrundavan"
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
