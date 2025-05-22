3e96acd0dedef939f45b38cacf7f5796338c627d
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-05-22 05:12:22 +0000

    operations

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -19,10 +19,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let aNum = 33
 
 let stringNum = String(aNum)
-console.log(aNum);
-console.log(typeof(aNum));
-console.log(typeof(stringNum));
+// console.log(aNum);
+// console.log(typeof(aNum));
+// console.log(typeof(stringNum));
 
 // 33
 // number
 // string
+
+
+
+///////////////  operations ///////////
+
+let value= 3 
+let negValue = -value
+
+// console.log(negValue)
+
+
+// console.log(7%4);
+console.log(1 + "2");
+console.log(1 + "2"+2);
+console.log("2"+1);
+console.log(1+2 + "2");
+console.log(+"2"+1+2 );
+console.log("2"+1+2 );
+
+// 12
+// 122
+// 21
+// 32
+// 5
+// 212
+
+
