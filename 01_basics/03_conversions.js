43725b44dfa8211a132cecdcb85b1a2581710535
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-05-22 04:46:56 +0000

    conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,28 @@
+let score = "hari"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+
+
+let isLoggedIn = "ram"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// null , 0, undefined => false
+// anynumber,1,and string => true
+
+
+let aNum = 33
+
+let stringNum = String(aNum)
+console.log(aNum);
+console.log(typeof(aNum));
+console.log(typeof(stringNum));
+
+// 33
+// number
+// string
