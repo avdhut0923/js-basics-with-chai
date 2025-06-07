6961f4e86b190e650401f2f0caf46acee2f7d350
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-06-07 07:54:36 +0000

    day 1 again

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,9 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// cmparisons convert null to a number (treat as 0) [>,<,>=,<=]
+ 
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,32 @@
+const score = 200
+const value = 23.9
+
+const isLoggedIn = false
+const outsideTemp = null
+let usrEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+// console.log(typeof(score))
+// console.log(typeof(value))
+// console.log(typeof(id))
+// console.log(typeof(outsideTemp))
+
+const bigNumber = 4099097654321n
+// console.log(typeof(bigNumber))
+
+const heros = ["Ram", "krishna", "Hari"];
+let myObj = {
+    name : "jai Hari",
+    age : 125,
+}
+
+const myFunction = function(){
+    console.log("jai jai ram jai jai ram jai jai ram krishna hari ")
+}
+myFunction();
+
+console.log(typeof anotherId);
+
