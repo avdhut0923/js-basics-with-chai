9f095e7ecd2d38bff156bf59ad6083270b4fb292
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-06-10 08:22:45 +0000

    end of basics 1

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,21 @@
+let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+
+// let mycreatedDate = new Date(2023 , 0, 23)
+// let mycreatedDate = new Date(2023 , 0, 23,5,3)
+// let mycreatedDate = new Date(2023 , 0, 23,5,3,1)
+let mycreatedDate = new Date("01-14-2025")
+
+// console.log(mycreatedDate.toLocaleString())
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth()+1)//to avoid 0 based indexing
+// console.log(newDate.getDay())
+
+
