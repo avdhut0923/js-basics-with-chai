9b2945e23775a70b7a5cacd2e095cf006c6fa4f0
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-06-11 08:47:55 +0000

    basics 2

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+const arr = [2,3,5,6,7,9]
+const heros =["krishna","Ram","hanuman","mahadev"]
+
+const arr2 = new Array(1,3,5,7,8)
+// console.log(arr[3]);
+
+// arr.push(12);
+// // console.log(arr)
+// arr.pop();
+// // console.log(arr)
+// arr.shift();
+// // console.log(arr)
+// arr.unshift(32);
+// // console.log(arr)
+
+// console.log(heros.includes(6))
+// console.log(heros.includes("Ram"))
+
+// console.log(arr.indexOf(7));
+
+// const newArr = arr.join()
+// console.log(newArr)
+// console.log(typeof(newArr))
+
+console.log("A", arr)
+const myn1 = arr.slice(1,3)
+console.log(myn1)
+
+console.log("B", arr)
+const myn2 = arr.splice(1,3)
+console.log(myn2)
+
+console.log("C",arr)
+console.log(myn2)
+
+
