90662865a0f1cd125f0dd9c340ce1cfc727b6f11
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-06-16 14:59:56 +0000

    eit

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -11,11 +11,28 @@ let mydate = new Date();
 // let mycreatedDate = new Date(2023 , 0, 23,5,3,1)
 let mycreatedDate = new Date("01-14-2025")
 
-// console.log(mycreatedDate.toLocaleString())
+console.log(mycreatedDate.toLocaleString())
 
 let newDate = new Date();
 // console.log(newDate);
 // console.log(newDate.getMonth()+1)//to avoid 0 based indexing
 // console.log(newDate.getDay())
+
+
+let myTimestamp = Date.now();
+
+console.log(myTimestamp);
+ // Log the time in milliseconds since January 1, 1970
+//    console.log(myTimestamp.getTime()); 
+console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date();
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+
+})
+console.log(newDate);
+
 
 
