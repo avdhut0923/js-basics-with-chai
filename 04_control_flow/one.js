bf6fa872f98d31d548490a8a512c6d0329e7ef52
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-06-19 21:54:49 +0530

    after basics

---
+++ 04_control_flow/one.js
@@ -0,0 +1,86 @@
+// const isLoggedIn = true;
+// if(isLoggedIn){
+//     console.log("bolo jai shree ram");
+
+// }
+// else console.log("koi baat nahi ")
+
+
+// const balance = 2000 ;
+
+// if(balance>200) console.log("you are allowed");
+
+// if(balance<500) console.log("less than 500");
+// else if(balance<1000) console.log("less than 1000");
+// else if(balance<1500) console.log("less than 1500");
+// else  console.log("less than 2500");
+
+
+// const userLoggedIn = false;
+// const debitCard = true ;
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+
+// if(userLoggedIn && debitCard && 2==2 ){
+//     console.log("allow to buy course");
+// }else
+// {
+//     console.log("you are not allowed")
+// }
+// if(userLoggedIn || loggedInFromEmail){
+//     console.log("user is logged in ");
+// }
+
+
+
+// const month = "2";
+
+// switch (month){//if we dont use break // in case of successful execution of any cases exexution is going on till end execpt for default (fall through)
+//     case "sep":
+//         console.log("month is september");
+//         break;
+//     case "aug":
+//         console.log("month is augest");
+//         break;
+//     case "jan":
+//         console.log("month is january");
+//         break;
+//     case "feb":
+//         console.log("month is february");
+//         break;
+//     default : 
+//       console.log("something is wrong there")    ;
+// }
+
+
+
+const userEmail = [];
+if(userEmail){
+    console.log("got user email");
+}
+else {
+    console.log("don't have user email");
+
+}
+
+// falsy values 
+
+// false , 0 , -0, BigInt 0n , null , undefined , NaN
+
+// rest all are truthy 
+
+// "false" , "0"," " , [] , {}  , function() {} etc....
+
+
+// to check empty array 
+if(userEmail.length === 0 ){
+    // console.log("array is empty") ;
+}
+
+// to check empty object
+const obj = {}
+
+if(Object.keys(obj).length === 0 ){
+        console.log("object is empty") ;
+
+}
